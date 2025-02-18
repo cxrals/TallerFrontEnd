39267acbcc71dc1be0ffed9080db6d9826d0e046
Carolina Larroque <55190406+cxrals@users.noreply.github.com>
2025-02-18 15:33:25 -0300

    componente tiempo total

--- src/components/TiempoTotal.jsx
+++ src/components/TiempoTotal.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { useSelector } from "react-redux"
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const TiempoTotal = () => {
+  const sesiones = useSelector(state => state.sesiones.listaSesiones)
+  const tiempoTotal = sesiones.reduce((total, sesion) => total + Number(sesion.tiempo), 0);
+  
   return (
     <div className="card">
         <h5 className="card-header">Tiempo Total (minutos)</h5>
         <div className="card-body">
-            <p className="numeroTotales">30</p>
+            <p className="numeroTotales">{tiempoTotal}</p>
         </div>
     </div>
   )
